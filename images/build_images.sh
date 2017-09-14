a3b25527c77d2b995058c698ea839b3c573a31d1
Alexander Kernozhitsky <sh200105@mail.ru>
2017-09-14 21:51:36 +0300

    Added role images

---
+++ images/build_images.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function build_one {
+	inkscape "$1.svg" -e "$1.png" -w "$2"
+	rm -f "../data/$1.png"
+	cp "$1.png" "../data"
+}
+
+build_one "userAdmin"   64
+build_one "userOwner"   64
+build_one "userSimple"  64
+build_one "userBlocked" 64
