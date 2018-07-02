f1011149c831aad178167bfa5f033de669d59e26
Alexander Kernozhitsky <sh200105@mail.ru>
2018-07-02 23:39:53 +0300

    Minor fixes

--- build/image/files/start-tsweb.sh
+++ build/image/files/start-tsweb.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [ -z "`ls /var/tsweb --hide='lost+found'`" ] 
-then
+if [ -z "$(ls /var/tsweb --hide='lost+found')" ]; then
+	# TODO: Add support for auto-generating passwords on first run!
 	echo 'Admin account created!'
 	echo 'Login: admin'
 	echo 'Password: password'
-
+	
 	rsync -a /etc/.copy/var/tsweb/* /var/tsweb
 fi
 
