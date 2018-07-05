3f25e391b1fee16225123fafff59d0588cc0e998
Alexander Kernozhitsky <sh200105@mail.ru>
2018-07-05 19:14:36 +0300

    Updated Tester

--- build/build_all.sh
+++ build/build_all.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Builds everything that is necessary after cloning Tester Web from a git repo
 
+cd ../tester &&
+make build-timerlib &&
+cd ../build &&
 ./build_project.sh &&
 ./build_package.sh
--- build/builder/files/start-build.sh
+++ build/builder/files/start-build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-cd tester/timerlib \
-	&& bash build-linux.sh \
-	&& cd ../../build \
-	&& bash build_all.sh
+cd build &&
+bash build_all.sh
