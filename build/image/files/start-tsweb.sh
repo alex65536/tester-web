bcec54f532ea9e5ad377b0eb74998b9960826929
Alexander Kernozhitsky <sh200105@mail.ru>
2018-07-05 18:57:25 +0300

    Updated Dockerfile for Docker image

--- build/image/files/start-tsweb.sh
+++ build/image/files/start-tsweb.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
 if [ -z "$(ls /var/tsweb --hide='lost+found')" ]; then
-	# TODO: Add support for auto-generating passwords on first run!
-	echo 'Admin account created!'
-	echo 'Login: admin'
-	echo 'Password: password'
-	
-	rsync -a /etc/.copy/var/tsweb/* /var/tsweb
+	cd /opt/tsweb/bin && ./tsweb-mkcfg
+	sed -i -E -e 's/^port=[0-9]+$/port=80/' /var/tsweb/data/config.ini
 fi
 
-cd /opt/tsweb/bin && ./tsweb-x86_64-linux
+cd /opt/tsweb/bin && ./tsweb
